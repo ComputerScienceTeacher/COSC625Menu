f803872445b238b434963a10d3a884fffa446cc0
Nnabugw <nonyirimba@worcester.edu>
2021-11-04 06:37:12 -0400

    Update ClassHistory.java

--- COSC625/src/MenuFunctions/ClassHistory.java
+++ COSC625/src/MenuFunctions/ClassHistory.java
@@ -2,9 +2,11 @@ package MenuFunctions;
 
 public class ClassHistory {
 
-	public ClassHistory() {
-		System.out.println("Success! Class: ");
-		System.out.print("ClssHistory" + " works!");
+	public String ClassHistory() {
+		//System.out.println("Success! Class: ");
+		//System.out.print("ClssHistory" + " works!");
+                String nanax = "Success! Class: " + "ClassHistory" + " works!";
+		return nanax; 
 	}
 
 }
