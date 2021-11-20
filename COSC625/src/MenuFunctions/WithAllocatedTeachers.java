90b8991ca032d9aae16f6a77b4b5313547b351cf
sahoos21 <ssahoo0@frostburg.edu>
2021-11-20 18:20:58 -0500

    Update WithAllocatedTeachers.java

--- COSC625/src/MenuFunctions/WithAllocatedTeachers.java
+++ COSC625/src/MenuFunctions/WithAllocatedTeachers.java
@@ -1,33 +1,11 @@
-/**
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- */
+package MenuFunctions;
+
+import project.RAPMenu;
+
+public class WithAllocatedTeachers {
+
+	public WithAllocatedTeachers(RAPMenu rp1) {
+		rp1.getMainLabel().setText("Success! WithAllocatedTeachers works!");
+	}
+
+}
